e90e95fd94a9f2c61a32f2b7b3143b256324af2a
Molly Lin <mollyslin@gmail.com>
2022-03-27 17:18:51 -0700

    made testy_mctest prettier

--- testy_mctest.js
+++ testy_mctest.js
@@ -1,29 +1,16 @@
-//print
-console.log("hello")
+// print
+console.log("hello");
+
 // fizzbuzz
 // for numbers 1-100 for each number divisable by 3 print fizz for each number divisable by 5 print buzz for numbers divisable by both 3 and 5 print fizzbuzz. if the following conditions aren't met print the number
-// google js for loop and how to do this. search for javascript tutorial. codeacademy? launchschool
-//data analytics in python. google pandas
-
-/*
 for (let i = 1; i <= 100; i++) {
-	if (i % 3 == 0) {
-		console.log("fizz")
-	} else if (i % 5 ==0){
-		console.log("buzz")
+	if (i % 3 == 0 && i % 5 == 0) {
+		console.log("fizzbuzz");
+	} else if (i % 3 == 0) {
+		console.log("fizz");
+	} else if (i % 5 == 0) {
+		console.log("buzz");
 	} else {
-		console.log(i)
+		console.log(i);
 	}
-}
-*/
-
-for (let i = 1; i<=100; i++){
-	if (i % 3 ==0 && i % 5 ==0) {
-		console.log ("fizzbuzz")
-	} else if (i % 3 ==0) {
-		console.log("fizz")
-	} else if (i % 5 ==0) {
-		console.log("buzz")
-	}
-	else {console.log(i)}
 }
